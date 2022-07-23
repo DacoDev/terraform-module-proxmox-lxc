815df546449c362aab4b796c1ae49901cf7be252
Dale Coyner <daco@dacodev.com>
2022-07-23 17:02:23 -0400

    fix: update examples

--- examples/debian-11-single.tf
+++ examples/debian-11-single.tf
@@ -5,7 +5,7 @@ module "3_debian_lxcs" {
   container_name          = "debian-k8s-lxc"
   node_name               = "example"
   container_template_file = "http://download.proxmox.com/images/system/debian-11-standard_11.3-1_amd64.tar.zst"
-  cores                   = 2
+  cpu_cores               = 2
   ram_MiB                 = 2000
   swap_MiB                = 2000
   vm_id                   = 1000
